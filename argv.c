e08eb142aad7ae75005112a9da049e9f1759cf7c
Briano Goestiawan <b@briano.io>
2021-08-19 00:45:24 +1000

    formatting

--- argv.c
+++ argv.c
@@ -1,9 +1,8 @@
 // Argv prints the argument list.
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-    for (int i=0; i<argc; i++) {
-        printf("%d\t%s\n", i, argv[i]);
-    }
+int main(int argc, char **argv) {
+	for (int i=0; i<argc; i++) {
+		printf("%d\t%s\n", i, argv[i]);
+	}
 }
